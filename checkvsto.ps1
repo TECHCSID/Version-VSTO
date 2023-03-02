bbc5fbbd6a6290e8fdb7b2ebdee636c3bbb0443e
TECHCSID <30695095+TECHCSID@users.noreply.github.com>
2023-03-02 12:41:07 +0100

    Update checkvsto.ps1

--- checkvsto.ps1
+++ checkvsto.ps1
@@ -1,2 +1,2 @@
 $version = Get-ChildItem HKCU:\Software\Microsoft\Windows\CurrentVersion\Uninstall | foreach-object {Get-ItemProperty $_.PsPath} | ? { $_.displayname -match 'GenApi.iNot.Outlook' } 
-$version.DisplayVersion
+write-output $version.DisplayVersion
